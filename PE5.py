072ba308ce6a062bf6224cecc5e1aceb80a21485
Shpoogtaz <93548948+Shpoogatz@users.noreply.github.com>
2022-03-07 16:11:32 -0500

    Update PE5.py

--- PE5.py
+++ PE5.py
@@ -12,3 +12,13 @@ while i < 21:
 print(num)'''
 
 # the better solution...
+check_list = [11, 13, 14, 16, 17, 18, 19, 20]
+
+# we input the step as the highest number that we want to check for
+def find_solution(max(check_list)):
+    for num in range(step, 999999999, step): # did 99999999 beacause just assuming the val is less than that
+        if all(num % n == 0 for n in check_list): # if all numbers in the checklist divide into the current num
+            return num # return the num
+    return None
+
+solution = find_solution(20)
