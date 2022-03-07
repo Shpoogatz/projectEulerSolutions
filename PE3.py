4371c99ec5b4863bbca847931276ea5e331bc648
Shpoogtaz <93548948+Shpoogatz@users.noreply.github.com>
2022-03-07 15:22:15 -0500

    Update PE3.py

--- PE3.py
+++ PE3.py
@@ -1,13 +1,13 @@
-# this file contains a few solutions to the third project euler problem
+# https://levelup.gitconnected.com/solve-and-understand-the-project-euler-problems-in-python-introduction-and-problems-1-3-32625d1633f7
 
 # the first algorithm is buggy with squares and does not return the correct answer
 # it is also the fatest least demanding
 
-n = 600851475143
-i = 2
-while i * i < n:
-    while n % i == 0:
-        n = n / i
-    i = i + 1
+number = 600851475143
 
-print n
+for prime_factor in range(2, int(math.sqrt(number)) + 1):
+    while number % prime_factor == 0:
+        number = number / prime_factor
+        if number == 1 or number == prime_factor:
+            print(prime_factor)
+            break
