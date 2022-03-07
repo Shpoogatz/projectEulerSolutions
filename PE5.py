15e6b2729f811211ae855a39eb59756caa040bea
Shpoogtaz <93548948+Shpoogatz@users.noreply.github.com>
2022-03-07 15:54:46 -0500

    Create PE5.py

---
+++ PE5.py
@@ -0,0 +1,14 @@
+# multiple solutions to PE 5
+
+# this worlds but takes forever becasue the loop can get up to 19 before realizing it does not work
+'''num = 2520
+i = 2
+while i < 21:
+    if num % i == 0:
+        i += 1
+    else:
+        num += 1
+        i = 2
+print(num)'''
+
+# the better solution...
